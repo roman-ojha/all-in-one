3a34ee2218221bc3435d586595b3cbf66f781db7
Roman-Ojha <razzroman98@gmail.com>
2022-08-03 10:04:17 +0545

    feat(script): connect adb android wirelessly

--- run.ps1
+++ run.ps1
@@ -10,3 +10,11 @@ elseif($args -eq "deactivate"){
     deactivate
     cd "../../../"
 }
+elseif($args[0] -eq "adb"){
+    # connect adb android wirelessly
+    $str1=$args[1]
+    $str2=":5555"
+    $final=$str1+$str2
+    adb tcpip 5555
+    adb connect $final
+}
